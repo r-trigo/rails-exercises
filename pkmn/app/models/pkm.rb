1ffc94923bb86eae7fc24da157ae1f36674f9cfc
ruimigueltrigo <ruimigueltrigo@gmail.com>
2017-01-12 09:52:14 +0000

    fix search by select

--- pkmn/app/models/pkm.rb
+++ pkmn/app/models/pkm.rb
@@ -2,7 +2,7 @@ class Pkm < ApplicationRecord
   belongs_to :region
 
   def self.search(region_id)
-    if region_id != ''
+    if region_id && region_id != ''
       Pkm.where(['region_id = ?', region_id])
     else
       Pkm.all
