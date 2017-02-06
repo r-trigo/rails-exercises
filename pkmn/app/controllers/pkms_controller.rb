3f0e2af96a9bc92b164ddf495f757b8dda5976eb
ruimigueltrigo <ruimigueltrigo@gmail.com>
2017-02-06 17:54:40 +0000

    friendly_id

--- pkmn/app/controllers/pkms_controller.rb
+++ pkmn/app/controllers/pkms_controller.rb
@@ -75,7 +75,9 @@ class PkmsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_pkm
-    @pkm = Pkm.find(params[:id])
+    #@pkm = Pkm.find(params[:id])
+    #friendly_id
+    @pkm = Pkm.friendly.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
