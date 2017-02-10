2d9a2781969ba13a189a33ff68a5f69ee26bb9a8
ruimigueltrigo <ruimigueltrigo@gmail.com>
2017-02-10 12:26:59 +0000

    get geotag

--- TrainingDay/app/src/main/java/pt/edp/trainingday/ImageryActivity.java
+++ TrainingDay/app/src/main/java/pt/edp/trainingday/ImageryActivity.java
@@ -3,23 +3,24 @@ package pt.edp.trainingday;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Camera;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
-import android.os.Build;
+import android.media.ExifInterface;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class ImageryActivity extends AppCompatActivity {
 
     private ImageButton ib_foto1;
     private ImageButton ib_foto2;
-    private Button bu_enviar;
+    private TextView tv_lat_foto1;
+    private TextView tv_lng_foto1;
     private final static int REQUEST_CODE = 200;
     private boolean foto_1;
 
@@ -34,6 +35,9 @@ public class ImageryActivity extends AppCompatActivity {
             ib_foto2.setEnabled(false);
             Toast.makeText(this, "O dispositivo não possui câmara", Toast.LENGTH_LONG).show();
         }
+
+        tv_lat_foto1 = (TextView) findViewById(R.id.textView_latitude_foto1);
+        tv_lng_foto1 = (TextView) findViewById(R.id.textView_longitude_foto1);
 
         ib_foto1 = (ImageButton) findViewById(R.id.imageButton_foto1);
         ib_foto1.setOnClickListener(new View.OnClickListener() {
@@ -56,14 +60,6 @@ public class ImageryActivity extends AppCompatActivity {
 
             }
         });
-
-        bu_enviar = (Button) findViewById(R.id.button_enviar_fotos);
-        bu_enviar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 
     @Override
@@ -81,10 +77,22 @@ public class ImageryActivity extends AppCompatActivity {
                 } else {
                     ib_foto2.setImageBitmap(bmp);
                 }
+                WriteTag(bmp);
 
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "Cancelada", Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    private void WriteTag(Bitmap bmp) {
+        /*OutputStream os = new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+
+            }
+        };
+        bmp.compress(Bitmap.CompressFormat.JPEG, 90, os);
+        ExifInterface ei = new ExifInterface(bmp);*/
     }
 }
