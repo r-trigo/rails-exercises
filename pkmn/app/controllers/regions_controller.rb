bbb3245915bd89e423cff84103bfe9d462fca0c4
ruimigueltrigo <ruimigueltrigo@gmail.com>
2017-03-06 11:35:35 +0000

    autocomplate ajax

--- pkmn/app/controllers/regions_controller.rb
+++ pkmn/app/controllers/regions_controller.rb
@@ -2,7 +2,7 @@ class RegionsController < ApplicationController
 
   def index
     @regions = Region.order(:name).where("name like ?", "%#{ params[:term] }%")
-    render json: @categories.map(&:name)
+    render json: @regions.map(&:name)
   end
 
 end
